d504e36fcc923ecb6b14c9c509013a52eebe1103
MikePHaynes <86630042+MikePHaynes@users.noreply.github.com>
2023-01-04 15:55:31 -0800

    Added basic user model

---
+++ src/main/java/com/mph/bugtrackerserver/model/User.java
@@ -0,0 +1,20 @@
+package com.mph.bugtrackerserver.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
