ae118bfc7d3630f42cb97f4bb780b169f0af68c6
MikePHaynes <86630042+MikePHaynes@users.noreply.github.com>
2023-01-04 22:31:25 -0800

    Updated project controller/service classes

--- src/main/java/com/mph/bugtrackerserver/controller/ProjectController.java
+++ src/main/java/com/mph/bugtrackerserver/controller/ProjectController.java
@@ -5,9 +5,9 @@ import com.mph.bugtrackerserver.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProjectController {
@@ -19,5 +19,16 @@ public class ProjectController {
     public ResponseEntity<Project> saveProject(@RequestBody Project project) {
         Project savedProject = projectService.saveProject(project);
         return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/project")
+    public ResponseEntity<Project> getProject(@RequestParam(name = "projectId") long projectId) {
+        Project project = projectService.getProject(projectId);
+        return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("/projects")
+    public List<Project> getProjects() {
+        return projectService.getProjects();
     }
 }
--- src/main/java/com/mph/bugtrackerserver/service/ProjectService.java
+++ src/main/java/com/mph/bugtrackerserver/service/ProjectService.java
@@ -5,6 +5,8 @@ import com.mph.bugtrackerserver.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProjectService {
 
@@ -13,5 +15,21 @@ public class ProjectService {
 
     public Project saveProject(Project project) {
         return projectRepository.save(project);
+    }
+
+    public Project getProject(long projectId) {
+        return projectRepository.findById(projectId).orElseThrow(() -> new RuntimeException("not found"));
+    }
+
+    public List<Project> getProjects() {
+        return projectRepository.findAll();
+    }
+
+    public Project updateProject(long projectId, Project project) {
+        Project existingProject = projectRepository.findById(projectId).orElseThrow(() -> new RuntimeException("not found"));
+        existingProject.setName(project.getName());
+        existingProject.setDescription(project.getDescription());
+        projectRepository.save(existingProject);
+        return existingProject;
     }
 }
